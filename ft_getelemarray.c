6228ccc2084020c72428ba4b5a6e50717bf6e9a3
emmflo <emmanuelflo@hotmail.fr>
2016-11-16 17:27:31 +0100

    getelemarray/newarray

--- ft_getelemarray.c
+++ ft_getelemarray.c
@@ -6,7 +6,7 @@
 /*   By: eflorenz <eflorenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 00:32:55 by eflorenz          #+#    #+#             */
-/*   Updated: 2016/11/16 02:41:29 by eflorenz         ###   ########.fr       */
+/*   Updated: 2016/11/16 17:21:02 by emmflo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_getelemarray(void *tab, size_t n, size_t *dimensions,
 	size_t	i;
 	int		index;
 
+	if (tab == NULL || dimensions == NULL || human_index == NULL)
+		return (NULL);
 	if (n == 0)
 		return (tab);
 	i = 1;
--- ft_newarray.c
+++ ft_newarray.c
@@ -6,7 +6,7 @@
 /*   By: eflorenz <eflorenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 20:10:55 by eflorenz          #+#    #+#             */
-/*   Updated: 2016/11/16 02:23:13 by eflorenz         ###   ########.fr       */
+/*   Updated: 2016/11/16 17:18:54 by emmflo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_newarray(size_t n, size_t *dimensions, size_t content_size)
 	size_t	i;
 	void	*tab;
 
+	if (dimensions == NULL)
+		return (NULL);
 	s = content_size;
 	i = 0;
 	while (i < n)
